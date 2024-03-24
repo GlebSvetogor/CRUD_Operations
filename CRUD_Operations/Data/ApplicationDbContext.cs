b111ca63c360a20c69b8824bba59210f5ac7c1c7
Gleb Svetogor <asabsiu@mail.ru>
2024-03-25 00:50:08 +0300

    Create Data and Entity folders

---
+++ CRUD_Operations/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Operations.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+    }
+}
--- CRUD_Operations/Program.cs
+++ CRUD_Operations/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var app = builder.Build();  
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,10 +12,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
+            
 app.UseStaticFiles();
-
+            
 app.UseRouting();
 
 app.UseAuthorization();
